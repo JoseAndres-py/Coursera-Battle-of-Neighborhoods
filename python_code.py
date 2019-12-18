8ae04d04a4bb5dcdd0c18b745a9450c48812173c
josean11-py <josean.lopezm@gmail.com>
2019-12-18 14:17:25 -0500

    V2.0

--- python_code.py
+++ python_code.py
@@ -2,5 +2,4 @@
 import numpy as np  # useful for many scientific computing in Python
 import pandas as pd # primary data structure library
 
-print(Hello Capstone Project Course!)
-
+print('Hello Capstone Project Course!'')
