6596c1c97e7924b61c9cdd9e38d7dd3be6b8e63e
Jose Andres Lopez Munoz <Jose.Lopez2@ibm.com>
2019-12-18 14:01:09 -0500

    Primer py en git

---
+++ python_code.py
@@ -0,0 +1,6 @@
+# Este proyecto es "Capstone project" Coursera
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+
+print(Hello Capstone Project Course!)
+
